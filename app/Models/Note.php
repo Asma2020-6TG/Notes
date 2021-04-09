4246b624fcd1dff62b1b66181f21971b09ab2c04
unknown <archimilliard@gmail.com>
2021-04-09 21:47:35 +0200

    creating model for notes

---
+++ app/Models/Note.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    protected $table ="notes";
+    protected $fillable=['title','content','details'];
+    protected $hidden=['added_at'];
+    use HasFactory;
+}
